eec0c797debc3800dd69f5463dd4753a0881560e
russell Hofvendahl <rhofvendahl@gmail.com>
2020-08-14 18:51:15 -0700

    count as mobile if EITHER width OR height <=800px

--- common/static/common/js/main.js
+++ common/static/common/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     console.log('hey');
-    if (window.innerWidth <= 800) {
+    if (window.innerWidth <= 800 || window.innerHeight <= 800) {
         $('body').append($('#phone-screen > *'));
         $('#phone').hide();
     }
