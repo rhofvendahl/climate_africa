03846f5fc794eb6320396e9deba1e5522f1a9658
russell Hofvendahl <rhofvendahl@gmail.com>
2020-08-29 12:48:53 -0700

    add basic data to map

--- map/views.py
+++ map/views.py
@@ -1,30 +1,53 @@
 from django.shortcuts import render, redirect
+
 import folium
 import os
 import json
 import requests
-# import open
+
+def get_feature_tuples():
+    feature_tuples = []
+
+    floods_green_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static/map/data/floods_2020_08_29_green.geojson')
+    floods_green_file = open(floods_green_path)
+    floods_green_dict = json.load(floods_green_file)
+    floods_green_tuples = [(feature['properties']['latitude'], feature['properties']['longitude'], feature['properties']['alertlevel'], 'flood', feature['properties']['iconitemlink'],) for feature in floods_green_dict['features']]# if 'latitude' in feature and 'longitude' in feature]
+    feature_tuples += floods_green_tuples
+
+    floods_orange_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static/map/data/floods_2020_08_29_orange.geojson')
+    floods_orange_file = open(floods_orange_path)
+    floods_orange_dict = json.load(floods_orange_file)
+    floods_orange_tuples = [(feature['properties']['latitude'], feature['properties']['longitude'], feature['properties']['alertlevel'], 'flood', feature['properties']['iconitemlink'],) for feature in floods_orange_dict['features']]# if 'latitude' in feature and 'longitude' in feature]
+    feature_tuples += floods_orange_tuples
+
+    droughts_green_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static/map/data/droughts_2020_08_29_green.geojson')
+    droughts_green_file = open(droughts_green_path)
+    droughts_green_dict = json.load(droughts_green_file)
+    droughts_green_tuples = [(feature['properties']['latitude'], feature['properties']['longitude'], feature['properties']['alertlevel'], 'drought', feature['properties']['iconitemlink'],) for feature in droughts_green_dict['features']]# if 'latitude' in feature and 'longitude' in feature]
+    feature_tuples += droughts_green_tuples
+
+    droughts_orange_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static/map/data/droughts_2020_08_29_orange.geojson')
+    droughts_orange_file = open(droughts_orange_path)
+    droughts_orange_dict = json.load(droughts_orange_file)
+    droughts_orange_tuples = [(feature['properties']['latitude'], feature['properties']['longitude'], feature['properties']['alertlevel'], 'drought', feature['properties']['iconitemlink'],) for feature in droughts_orange_dict['features']]# if 'latitude' in feature and 'longitude' in feature]
+    feature_tuples += droughts_orange_tuples
+
+    return list(set(feature_tuples))
+
+def get_feature_dicts():
+    feature_tuples = get_feature_tuples()
+    return [{'latitude': feature_tuple[0], 'longitude': feature_tuple[1], 'alertlevel': feature_tuple[2], 'type': feature_tuple[3], 'icon_link': feature_tuple[4]} for feature_tuple in feature_tuples]
 
 def init(request):
     return redirect('map:map_test')
 
 def map_test(request):
-    # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    # drought_geojson = os.path.join(BASE_DIR, 'data/drought/drought.geojson')
-    # drought_topo = os.path.join(BASE_DIR, 'data/drought/drought.json')
     map = folium.Map(location=[5.273, 16.821], min_zoom=2, max_zoom=9, zoom_start=3)
-
-    # print(json.load(open(drought_topo)))
-    # folium.GeoJson(
-    #     drought_geojson,
-    #     name='geojson'
-    # ).add_to(map)
-
-    # folium.TopoJson(
-    #     open(drought_topo),
-    #     'objects.gpcc_spi72_drought',
-    #     name='topojson'
-    # ).add_to(map)
+    feature_dicts = get_feature_dicts()
+    for feature_dict in feature_dicts:
+        icon = folium.features.CustomIcon(feature_dict['icon_link'], icon_size=(32,32,))
+        marker = folium.Marker([feature_dict['latitude'], feature_dict['longitude']], icon=icon)
+        marker.add_to(map)
 
     context = {
         'map_iframe': map._repr_html_(),
