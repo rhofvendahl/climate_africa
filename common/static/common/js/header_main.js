2d63207856e0e9d4518d31040d7dd5d43ff7bf74
russell Hofvendahl <rhofvendahl@gmail.com>
2020-10-03 10:47:50 -0700

    update header behavior

--- common/static/common/js/header_main.js
+++ common/static/common/js/header_main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     var animating = false;
+    $('.container-fluid > div:first-child').css({top: '6rem'});
 
     var hideHeader = function() {
         if (!animating) {
@@ -11,12 +12,9 @@ $(document).ready(function() {
             });
             $('.container-fluid > div:first-child').animate({top: '0'}, 400);
             $('#return-header').fadeIn(400);
-            // $('#return-header').animate({marginTop: ".7rem"}, 400);
-            // headerHidden = true;
         }
     }
 
-    $('.container-fluid > div:first-child').css({top: '6rem'});
     var showHeader = function() {
         if (!animating) {
             $('#header').show();
@@ -28,31 +26,23 @@ $(document).ready(function() {
             $('.container-fluid > div:first-child').animate({top: '6rem'}, 400);
             $('#return-header').fadeOut(400);
 
-            // $('#return-header').animate({marginTop: "-2.5rem"}, 400, function() {
-            //     $('#return-header').hide();
-            // });
-            // headerHidden = false;
-
-            // setTimeout(hideHeader, 4000);
-            if (hideHeaderTimeout) {
-                clearTimeout(hideHeaderTimeout);
-                hideHeaderTimeout = null;
-                hideHeaderTimeout = setTimeout(hideHeader, 4000);
-            }
+            // if (hideHeaderTimeout) {
+            //     clearTimeout(hideHeaderTimeout);
+            //     hideHeaderTimeout = null;
+            //     hideHeaderTimeout = setTimeout(hideHeader, 4000);
+            // }
         }
     }
 
-    hideHeaderTimeout = setTimeout(hideHeader, 5000);
+    // hideHeaderTimeout = setTimeout(hideHeader, 5000);
 
     // Firefox
     $('html').on('DOMMouseScroll', function(event) {
         var delta = event.originalEvent.detail;
 
         if (delta > 0) {
-            // console.log('scrollup', headerHidden);
             showHeader();
         } else {
-            // console.log('scrolldown', headerHidden);
             hideHeader(0);
         }
     });
@@ -62,12 +52,8 @@ $(document).ready(function() {
         var delta = event.originalEvent.wheelDelta;
 
         if (delta > 0) {
-            // console.log('scrollup')
             showHeader();
-            // console.log('about to clear', hideHeaderTimeout)
-            // console.log('cleared', hideHeaderTimeout)
         } else {
-            // console.log('scrolldown')
             hideHeader();
         }
     });
