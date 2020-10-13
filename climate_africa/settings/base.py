34b49eab66703422a90b7731df42c467d57bef04
russell Hofvendahl <rhofvendahl@gmail.com>
2020-10-13 12:37:32 -0700

    mess with base_dir

--- climate_africa/settings/base.py
+++ climate_africa/settings/base.py
@@ -1,7 +1,7 @@
 print('BASE SETTINGS IMPORTED')
 
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import environ
 env = environ.Env(DEBUG=(bool, False))
