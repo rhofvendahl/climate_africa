f3603fa60b52a950adf891eb3b9954f7179af26f
russell Hofvendahl <rhofvendahl@gmail.com>
2021-01-14 12:03:16 -0800

    remove empty public app

--- public/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from public import views
-
-app_name = 'public'
-
-urlpatterns = [
-    # path('', views.init, name='init'),
-    path('login/', views.login, name='login'),
-]
